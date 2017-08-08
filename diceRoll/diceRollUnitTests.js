820f55b5386e599d25656f578923972cdb4d109c
Ender Smith <ender@happyleviathan.com>
2017-08-07 21:19:21 -0700

    renamed ProtoTest() to UnitTest()

--- diceRoll/diceRollUnitTests.js
+++ diceRoll/diceRollUnitTests.js
@@ -3,33 +3,34 @@ function runTests(runTF) {
     document.getElementById('dispIn').innerHTML = 'runTest == true';
     document.getElementById('dispOut').innerHTML = 'runTest == true';
     var passFail = [
-      new ProtoTest('randomIntByDice', randomIntByDice('d1'), 1),
-      new ProtoTest('subRandomIntForDice', subRandomIntForDice('d1 + 3'), '(1) + 3'),
-      new ProtoTest('arrayToEquation', arrayToEquation([2,'+',700]), '2+700'),
-      new ProtoTest('printToInnerHTML-replace', printToInnerHTML('forUnitTests', 'test', true), 'test'),
-      new ProtoTest('printToInnerHTML-append', printToInnerHTML('forUnitTests', ' test', false), 'test test'),
-      new ProtoTest('clearScreen-inputLine', clearScreen('dispOut'), ''),
-      new ProtoTest('clearScreen-outputLine', clearScreen('dispIn'), ''),
-      new ProtoTest('key_first', key_first('1'), '1,1'),
-      new ProtoTest('key_numberAfterNumber', key_numberAfterNumber('0'), '10,1'),
-      new ProtoTest('key_diceAfterNumber', key_diceAfterNumber('d20'), '10,19'),
-      new ProtoTest('key_operator', key_operator('+'), '+,20'),
-      new ProtoTest('clearInputArray', clearInputArray(), '0,0'),
-      new ProtoTest('keypadPress-first', keypadPress('1'), '1,1'),
-      new ProtoTest('keypadPress-numberAfterNumber', keypadPress('0'), '0,0'),
-      new ProtoTest('keypadPress-diceAfterNumber', keypadPress('d2'), 'd2,d2'),
-      new ProtoTest('keypadPress-diceAfterDice', keypadPress('d2'), 'd2,'),
-      new ProtoTest('keypadPress-numberAfterDice', keypadPress('2'), '2,'),
-      new ProtoTest('lazinessTest', clearScreen(clearScreen()), ''),
-      new ProtoTest('roll', roll(['d1','+','d1']), '2 [(1)+(1)] <br>'),
-      new ProtoTest('lazinessTest_redux', clearScreen(clearScreen()), '')
+      new UnitTest('randomIntByDice', randomIntByDice('d1'), 1),
+      new UnitTest('subRandomIntForDice', subRandomIntForDice('d1 + 3'), '(1) + 3'),
+      new UnitTest('arrayToEquation', arrayToEquation([2,'+',700]), '2+700'),
+      new UnitTest('printToInnerHTML-replace', printToInnerHTML('forUnitTests', 'test', true), 'test'),
+      new UnitTest('printToInnerHTML-append', printToInnerHTML('forUnitTests', ' test', false), 'test test'),
+      new UnitTest('clearScreen-inputLine', clearScreen('dispOut'), ''),
+      new UnitTest('clearScreen-outputLine', clearScreen('dispIn'), ''),
+      new UnitTest('key_first', key_first('1'), '1,1'),
+      new UnitTest('key_numberAfterNumber', key_numberAfterNumber('0'), '10,1'),
+      new UnitTest('key_diceAfterNumber', key_diceAfterNumber('d20'), '10,19'),
+      new UnitTest('key_operator', key_operator('+'), '+,20'),
+      new UnitTest('clearInputArray', clearInputArray(), '0,0'),
+      new UnitTest('keypadPress-first', keypadPress('1'), '1,1'),
+      new UnitTest('keypadPress-numberAfterNumber', keypadPress('0'), '0,0'),
+      new UnitTest('keypadPress-diceAfterNumber', keypadPress('d2'), 'd2,d2'),
+      new UnitTest('keypadPress-diceAfterDice', keypadPress('d2'), 'd2,'),
+      new UnitTest('keypadPress-numberAfterDice', keypadPress('2'), '2,'),
+      new UnitTest('lazinessTest', clearScreen(clearScreen()), ''),
+      new UnitTest('roll', roll(['d1','+','d1']), '2 [(1)+(1)] <br>'),
+      new UnitTest('lazinessTest_redux', clearScreen(clearScreen()), '')
     ];
+    return passFail;
   } else {
     return;
   }
 }
 
-function ProtoTest(testName, functionToBeTested, expectedOutput) {
+function UnitTest(testName, functionToBeTested, expectedOutput) {
   this.report;
   this.functionToBeTested = testName;
   this.functionReturned = functionToBeTested;
