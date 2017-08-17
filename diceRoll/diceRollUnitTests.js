0ea1db8b815b3838ab0e0ec33d533bf1cd4817c4
Ender Smith <ender@happyleviathan.com>
2017-08-17 15:54:40 -0700

    some unit tests added, UnitTest logging format improved

--- diceRoll/diceRollUnitTests.js
+++ diceRoll/diceRollUnitTests.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
   runTests(true);
 });
+var testGlobals = {
+  sumArray: [
+    {
+      count: 4,
+      dn: 'd6',
+      negative: false,
+      display: '4d6'
+    },
+    {
+      count: 2,
+      dn: '',
+      negative: true,
+      display: '-2'
+    }
+  ],
+  sumArray_display: '4d6-2'
+}
 
 function runTests(runTF) {
   if (runTF == true) {
+    console.log('MEMORY FUNCTION UNIT TESTS:')
     var passFail = [
-    // memory function unit tests
       new UnitTest('simulateFirstVisit()', function() {
           return (simulateFirstVisit(true) === '{}');
       }),
@@ -29,9 +46,54 @@ function runTests(runTF) {
         var saved = restoreDefaultSaveItems();
         saved = JSON.stringify(saved);
         return (saved === localStorage.saved);
+      })
+    ];
+    console.log('DATA MANIPULATION TESTS:')
+    passFail += [
+      new UnitTest('new Addend()', function() {
+        var blankAddend = new Addend();
+        var pass = true;
+        if (blankAddend.count !== 0) {
+          pass = false;
+        }
+        if (blankAddend.dn !== '') {
+          pass = false;
+        }
+        if (blankAddend.negative !== false) {
+          pass = false;
+        }
+        return pass;
       }),
-      // data manipulation tests
-
+      new UnitTest('addendChange()', function() {
+        var pass = true;
+        var workingAddend = addendChange('d4', '', true);
+        if (workingAddend.dn !== 'd4') {
+          pass = false;
+        }
+        workingAddend = addendChange(4, workingAddend);
+        if (workingAddend.count !== 4) {
+          pass = false;
+        }
+        workingAddend = addendChange('-', workingAddend);
+        if (workingAddend.negative !== true) {
+          pass = false;
+        }
+        workingAddend = addendChange('+', workingAddend);
+        if (workingAddend.negative !== false) {
+          pass = false;
+        }
+        return pass;
+      }),
+      new UnitTest('addendToDisplay()', function() {
+        var pass = true;
+        if (addendToDisplay(testGlobals.sumArray[0]) !== testGlobals.sumArray[0].display) {
+          pass = false;
+        }
+        if (addendToDisplay(testGlobals.sumArray[1]) !== testGlobals.sumArray[1].display) {
+          pass = false;
+        }
+        return pass;
+      }),
     ];
     clearScreen();
     clearScreen();
@@ -46,16 +108,14 @@ function UnitTest(testName, functionToBeTested) {
   this.functionToBeTested = functionToBeTested;
   this.report;
   try {
-    if (functionToBeTested() !== true) {
-      throw 'unexpected result';
+    if (!functionToBeTested()) {
+      throw '\t\tunexpected result';
     }
-    else{
-      this.report = ' PASS'
-    }
+    this.report = ' PASS';
   }
   catch(err) {
     this.report = ' FAIL:\n' + err;
   }
-  console.log(this.testName + this.report);
+  console.log('\t' + this.testName + this.report);
   return this.testName + this.report;
 }
