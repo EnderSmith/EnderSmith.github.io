fc3909d9036ee909207fd44f002a432aff976170
Ender Smith <ender@happyleviathan.com>
2017-09-14 18:55:23 -0700

    rearranged functions

--- diceRoll/diceRollUnitTests.js
+++ diceRoll/diceRollUnitTests.js
@@ -2,29 +2,48 @@ document.addEventListener('DOMContentLoaded', function() {
   runTests(true);
 });
 
-function runTests(runTF) {
-  if (runTF === true) {
-    var testArray = testList();
-
-    var passed = true;
-    var failedCount = 0;
-    var passedCount = 0;
-    for (var i = 0; i < testArray.length; i++) {
-      var context = new Context(preloadedSaveItems);
-      var app = new App(context);
-      var test = testArray[i];
-      test.run(app, this);
-      if (test.passed) {
-        passedCount++;
-      } else {
-        failedCount++;
-        passed = false;
+function UnitTest(testName, testFunction) {
+  return {
+    testName: testName,
+    testFunction: testFunction,
+    report: null,
+    result: null,
+    passed: false,
+    run: function(app) {
+      try {
+        this.result = this.testFunction(app, this);
+        if (this.result === true) {
+          this.passed = true;
+          return true;
+        }
+      } catch(err) {
+        this.report = ' ERROR: ' + err;
+        this.passed = false;
       }
-      console.log((test.passed ? 'PASSED: ' : 'FAILED: ') + test.testName + ': ' + test.report + ': ' + JSON.stringify(test.result || ''));
     }
-    console.log('\nTEST RUN ' + (passed ? 'PASSED ' : 'FAILED ') + '(Failed: ' + failedCount + '; Passed: ' + passedCount + ')');
+  };
+}
+
+function PlaceHolderFor(functionName, calledObject) {
+  return {
+    functionName: functionName,
+    calledObject: calledObject,
+    placeHolder: function() {
+      calledObject[functionName] = true;
+    }
+  }
+}
+
+function assert(condition, message) {
+  if (!condition) {
+    message = message || 'Assertion failed';
+    if (typeof Error !== 'undefined') {
+      var err = Error.name + ' ' + message;
+      throw err;
+    }
+    throw message;
   } else {
-    return;
+    return 'Assertion passed';
   }
 }
 
@@ -69,47 +88,28 @@ function testList() {
   ];
 }
 
-function PlaceHolderFor(functionName, calledObject) {
-  return {
-    functionName: functionName,
-    calledObject: calledObject,
-    placeHolder: function() {
-      calledObject[functionName] = true;
-    }
-  }
-}
+function runTests(runTF) {
+  if (runTF === true) {
+    var testArray = testList();
 
-function assert(condition, message) {
-  if (!condition) {
-    message = message || 'Assertion failed';
-    if (typeof Error !== 'undefined') {
-      var err = Error.name + ' ' + message;
-      throw err;
-    }
-    throw message;
-  } else {
-    return 'Assertion passed';
-  }
-}
-
-function UnitTest(testName, testFunction) {
-  return {
-    testName: testName,
-    testFunction: testFunction,
-    report: null,
-    result: null,
-    passed: false,
-    run: function(app) {
-      try {
-        this.result = this.testFunction(app, this);
-        if (this.result === true) {
-          this.passed = true;
-          return true;
-        }
-      } catch(err) {
-        this.report = ' ERROR: ' + err;
-        this.passed = false;
+    var passed = true;
+    var failedCount = 0;
+    var passedCount = 0;
+    for (var i = 0; i < testArray.length; i++) {
+      var context = new Context(preloadedSaveItems);
+      var app = new App(context);
+      var test = testArray[i];
+      test.run(app, this);
+      if (test.passed) {
+        passedCount++;
+      } else {
+        failedCount++;
+        passed = false;
       }
+      console.log((test.passed ? 'PASSED: ' : 'FAILED: ') + test.testName + ': ' + test.report + ': ' + JSON.stringify(test.result || ''));
     }
-  };
+    console.log('\nTEST RUN ' + (passed ? 'PASSED ' : 'FAILED ') + '(Failed: ' + failedCount + '; Passed: ' + passedCount + ')');
+  } else {
+    return;
+  }
 }
