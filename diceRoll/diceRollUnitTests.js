4d122caa43b23eab3e8108c0cb6e4d6f0ce82805
Ender Smith <ender@happyleviathan.com>
2017-09-19 12:19:05 -0700

    addDiceKeyListeners() unit test

--- diceRoll/diceRollUnitTests.js
+++ diceRoll/diceRollUnitTests.js
@@ -38,7 +38,7 @@ function assert(condition, message) {
   }
 }
 
-function stub() {
+function Stub() {
   var info = {
     called: {
       count: 0,
@@ -86,13 +86,13 @@ function stub() {
 function testList() {
   return [
     new UnitTest('run()', function(app, test) {
-      app.addRollBarListeners = stub();
-      app.addCalculatorListeners = stub();
-      app.addUserSaveButtonListener = stub();
-      app.simulateFirstVisit = stub();
-      app.checkMemory = stub();
-      app.toggleMenu = stub();
-      app.userSaveButtonCheckDisplay = stub();
+      app.addRollBarListeners = new Stub();
+      app.addCalculatorListeners = new Stub();
+      app.addUserSaveButtonListener = new Stub();
+      app.simulateFirstVisit = new Stub();
+      app.checkMemory = new Stub();
+      app.toggleMenu = new Stub();
+      app.userSaveButtonCheckDisplay = new Stub();
       app.run();
       assert(app.addRollBarListeners.info.called.check(), 'addRollBarListeners() not called');
       assert(app.addCalculatorListeners.info.called.check(), 'addCalculatorListeners() not called');
@@ -104,9 +104,9 @@ function testList() {
       return true;
     }),
     new UnitTest('addCalculatorListeners()', function(app, test) {
-      app.addNumberKeyListeners = stub();
-      app.addDiceKeyListeners = stub();
-      app.addOperatorKeyListeners = stub();
+      app.addNumberKeyListeners = new Stub();
+      app.addDiceKeyListeners = new Stub();
+      app.addOperatorKeyListeners = new Stub();
       app.addCalculatorListeners();
       assert(app.addNumberKeyListeners.info.called.check(), 'addNumberKeyListeners() not called');
       assert(app.addDiceKeyListeners.info.called.check(), 'addDiceKeyListeners() not called');
@@ -114,25 +114,38 @@ function testList() {
       return true;
     }),
     new UnitTest('addNumberKeyListeners()', function(app, test) {
-      app.context.attach = stub();
-      app.keypadPress = stub();
+      app.context.attach = new Stub();
+      app.keypadPress = new Stub();
       app.addNumberKeyListeners();
       assert(app.context.attach.info.called.check(), 'context.attach() not called');
       assert(app.context.attach.info.called.check(10), 'context.attach() not called 10 times');
-      assert(app.context.attach.info.args.all.check(0, 'num0', 'click', app.keypadPress.bind(this, 0)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(1, 'num1', 'click', app.keypadPress.bind(this, 1)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(2, 'num2', 'click', app.keypadPress.bind(this, 2)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(3, 'num3', 'click', app.keypadPress.bind(this, 3)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(4, 'num4', 'click', app.keypadPress.bind(this, 4)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(5, 'num5', 'click', app.keypadPress.bind(this, 5)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(6, 'num6', 'click', app.keypadPress.bind(this, 6)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(7, 'num7', 'click', app.keypadPress.bind(this, 7)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(8, 'num8', 'click', app.keypadPress.bind(this, 8)), 'attach() unexpected arguments');
-      assert(app.context.attach.info.args.all.check(9, 'num9', 'click', app.keypadPress.bind(this, 9)), 'attach() unexpected arguments');
+      assert(app.context.attach.info.args.all.check(0, 'num0', 'click', app.keypadPress.bind(this, 0)), 'attach() num0 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(1, 'num1', 'click', app.keypadPress.bind(this, 1)), 'attach() num1 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(2, 'num2', 'click', app.keypadPress.bind(this, 2)), 'attach() num2 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(3, 'num3', 'click', app.keypadPress.bind(this, 3)), 'attach() num3 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(4, 'num4', 'click', app.keypadPress.bind(this, 4)), 'attach() num4 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(5, 'num5', 'click', app.keypadPress.bind(this, 5)), 'attach() num5 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(6, 'num6', 'click', app.keypadPress.bind(this, 6)), 'attach() num6 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(7, 'num7', 'click', app.keypadPress.bind(this, 7)), 'attach() num7 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(8, 'num8', 'click', app.keypadPress.bind(this, 8)), 'attach() num8 unexpected arguments');
+      assert(app.context.attach.info.args.all.check(9, 'num9', 'click', app.keypadPress.bind(this, 9)), 'attach() num9 unexpected arguments');
       return true;
     }),
     new UnitTest('addDiceKeyListeners()', function(app, test) {
-      // return true;
+      app.context.attach = new Stub();
+      app.keypadPress = new Stub();
+      app.addDiceKeyListeners();
+      assert(app.context.attach.info.called.check(), 'context.attach() not called');
+      assert(app.context.attach.info.called.check(8), 'context.attach() not called 8 times');
+      assert(app.context.attach.info.args.all.check(0, 'd100', 'click', app.keypadPress.bind(this, 'd100')), 'attach() d100 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(1, 'd20', 'click', app.keypadPress.bind(this, 'd20')), 'attach() d20 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(2, 'd12', 'click', app.keypadPress.bind(this, 'd12')), 'attach() d12 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(3, 'd10', 'click', app.keypadPress.bind(this, 'd10')), 'attach() d10 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(4, 'd8', 'click', app.keypadPress.bind(this, 'd8')), 'attach() d8 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(5, 'd6', 'click', app.keypadPress.bind(this, 'd6')), 'attach() d6 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(6, 'd4', 'click', app.keypadPress.bind(this, 'd4')), 'attach() d4 unexpected arguments')
+      assert(app.context.attach.info.args.all.check(7, 'd2', 'click', app.keypadPress.bind(this, 'd2')), 'attach() d2 unexpected arguments')
+      return true;
     }),
     new UnitTest('addOperatorKeyListeners()', function(app, test) {
       // return true;
